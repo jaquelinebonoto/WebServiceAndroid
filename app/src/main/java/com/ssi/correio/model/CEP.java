7a863235c991b1c84e5b7b57f7adabe26d1bc3bd
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2020-03-12 09:33:35 -0300

    Editando a model porque usei uma API diferente do tutorial

--- app/src/main/java/com/ssi/correio/model/CEP.java
+++ app/src/main/java/com/ssi/correio/model/CEP.java
@@ -6,8 +6,8 @@ public class CEP {
     private String logradouro;
     private String complemento;
     private String bairro;
-    private String cidade;
-    private String estado;
+    private String localidade;
+    private String uf;
 
     public String getCep() {
         return cep;
@@ -41,20 +41,20 @@ public class CEP {
         this.bairro = bairro;
     }
 
-    public String getCidade() {
-        return cidade;
+    public String getLocalidade() {
+        return localidade;
     }
 
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
+    public void setLocalidade(String Localidade) {
+        this.localidade = localidade;
     }
 
-    public String getEstado() {
-        return estado;
+    public String getUf() {
+        return uf;
     }
 
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public void setUf(String uf) {
+        this.uf = uf;
     }
 
     @Override
@@ -63,8 +63,8 @@ public class CEP {
                 + "\nLogradouro: " + getLogradouro()
                 + "\nComplemento: " + getComplemento()
                 + "\nBairro: " + getBairro()
-                + "\nCidade:" + getCidade()
-                + "\nEstado: " + getEstado();
+                + "\nCidade:" + getLocalidade()
+                + "\nEstado: " + getUf();
     }
 
 }
