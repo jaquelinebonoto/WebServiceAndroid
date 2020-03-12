38faaae92c42781ee40cd72e7bf71bda2f164375
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2020-03-11 22:06:19 -0300

    Validação báscia do cep no service

--- app/src/main/java/com/ssi/correio/service/HttpService.java
+++ app/src/main/java/com/ssi/correio/service/HttpService.java
@@ -4,9 +4,24 @@ import android.os.AsyncTask;
 
 import com.ssi.correio.model.CEP;
 
+
+//parametros de AsyncTask:
+//1. enviado para a execução da classe
+//2. tipo de parametro no método onProgressUpdate
+//3. tipo do retorno da classe
 public class HttpService extends AsyncTask<Void, Void, CEP> {
+
+    //instanciando o cep e colocando dentro do construtor
+    private final String cep;
+
+    public HttpService(String cep) {
+        this.cep = cep;
+    }
     @Override
     protected CEP doInBackground(Void... voids) {
+        if (this.cep != null && this.cep.length() == 8) {
+            // realizar busca
+        }
         return null;
     }
 }
