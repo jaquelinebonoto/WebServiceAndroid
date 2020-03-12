2d0a6665d2e4d926f1504dfe754e57a6c631a280
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2020-03-11 21:46:01 -0300

    Buscando botão na main e add listener nele

--- app/src/main/java/com/ssi/correio/MainActivity.java
+++ app/src/main/java/com/ssi/correio/MainActivity.java
@@ -3,6 +3,8 @@ package com.ssi.correio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnBuscarCep = findViewById(R.id.btnMain_buscarCep);
+
+        btnBuscarCep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // buscar o CEP
+            }
+        });
     }
 }
